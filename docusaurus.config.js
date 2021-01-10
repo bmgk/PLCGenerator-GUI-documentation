8fc1a2a7d49fc869ebe36ca024bac53d0b31a243
ituriel26 <bartosz.golebiowski24@gmail.com>
2021-01-10 20:26:49 +0100

    test modification

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,12 +10,12 @@
 module.exports = {
   title: 'PLCGenerator',
   tagline: 'Documentation for PLCGenerator application',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://plcgenerator-documentation.netlify.app/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
+  organizationName: 'bartoszgolebiowski', // Usually your GitHub org/user name.
   projectName: 'PLCGenerator-GUI-documentation', // Usually your repo name.
   themeConfig: {
     navbar: {
@@ -33,7 +33,8 @@ module.exports = {
         },
         // Please keep GitHub link to the right for consistency.
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href:
+            'https://github.com/bartoszgolebiowski/PLCGenerator-GUI-documentation',
           label: 'GitHub',
           position: 'right',
         },
@@ -74,13 +75,7 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/bartoszgolebiowski/PLCGenerator-GUI-documentation/edit/main',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
--- src/pages/index.js
+++ src/pages/index.js
@@ -17,32 +17,50 @@ import styles from './styles.module.css';
 
 const features = [
   {
-    title: 'Easy to Use',
+    title: 'Titule 1',
     imageUrl: 'img/undraw_docusaurus_mountain.svg',
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Id minim irure cupidatat nostrud magna sunt commodo eiusmod exercitation
+        nostrud. Lorem nulla ut cupidatat id est laborum nulla veniam eiusmod ut
+        ullamco. Veniam ullamco cillum qui aliquip ullamco laboris Lorem
+        proident non. Culpa aute sit deserunt cupidatat ex exercitation Lorem
+        non dolor. Eiusmod ullamco eiusmod fugiat cupidatat proident aliquip id
+        aute. Amet laborum ex cillum sit est Lorem elit enim id dolore
+        consectetur. Irure non nisi cillum do. Qui eiusmod magna non ea occaecat
+        eu est quis. Ipsum adipisicing velit et irure.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Titule 2',
     imageUrl: 'img/undraw_docusaurus_tree.svg',
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Id minim irure cupidatat nostrud magna sunt commodo eiusmod exercitation
+        nostrud. Lorem nulla ut cupidatat id est laborum nulla veniam eiusmod ut
+        ullamco. Veniam ullamco cillum qui aliquip ullamco laboris Lorem
+        proident non. Culpa aute sit deserunt cupidatat ex exercitation Lorem
+        non dolor. Eiusmod ullamco eiusmod fugiat cupidatat proident aliquip id
+        aute. Amet laborum ex cillum sit est Lorem elit enim id dolore
+        consectetur. Irure non nisi cillum do. Qui eiusmod magna non ea occaecat
+        eu est quis. Ipsum adipisicing velit et irure.
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Titule 3',
     imageUrl: 'img/undraw_docusaurus_react.svg',
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Id minim irure cupidatat nostrud magna sunt commodo eiusmod exercitation
+        nostrud. Lorem nulla ut cupidatat id est laborum nulla veniam eiusmod ut
+        ullamco. Veniam ullamco cillum qui aliquip ullamco laboris Lorem
+        proident non. Culpa aute sit deserunt cupidatat ex exercitation Lorem
+        non dolor. Eiusmod ullamco eiusmod fugiat cupidatat proident aliquip id
+        aute. Amet laborum ex cillum sit est Lorem elit enim id dolore
+        consectetur. Irure non nisi cillum do. Qui eiusmod magna non ea occaecat
+        eu est quis. Ipsum adipisicing velit et irure.
       </>
     ),
   },
@@ -68,8 +86,8 @@ function Home() {
   const {siteConfig = {}} = context;
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="plc generator optimalization c# tia siemens">
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
